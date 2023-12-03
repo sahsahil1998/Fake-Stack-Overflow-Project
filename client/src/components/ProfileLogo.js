69f17980939e364d3c6fa110ce28e4b96947c155
vaish28 <madhekarvaishnavi@gmail.com>
2023-12-03 14:53:35 -0500

    disabled profile button

--- client/src/components/ProfileLogo.js
+++ client/src/components/ProfileLogo.js
@@ -1,16 +1,39 @@
 // ProfileLogo.js
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 import '../stylesheets/profileLogo.css'; // Import the stylesheet for styling
 
 const ProfileLogo = () => {
+
+  const [isAuthenticated, setIsAuthenticated] = useState(false); // Added state to track authentication
+
+  useEffect(() => {
+    // Fetch user session status on component mount
+    axios.get('http://localhost:8000/api/users/check-session', { withCredentials: true })
+        .then(response => {
+            console.log(response.data.user)
+            setIsAuthenticated(response.data.isLoggedIn);
+            console.log(response.data.isLoggedIn);
+            // console.log(isAuthenticated);
+
+        })
+        .catch(error => console.error('Error checking user session:', error));
+}, []);
+
   return (
     <div className="profile-logo">
-      <Link to="/userprofile">
-        {/* Add your logo or profile image */}
-        <img src="../assets/ProfileLogo.jpg" alt="User Profile" />
-      </Link>
+      
+      {isAuthenticated ? (
+                    <button><Link to="/userprofile">
+                    {/* Add your logo or profile image */}
+                    <img src="../assets/ProfileLogo.jpg" alt="User Profile" />
+                  </Link>
+                  </button>
+                ) : (
+                    <button disabled >User Profile</button>
+                )}
     </div>
   );
 };
