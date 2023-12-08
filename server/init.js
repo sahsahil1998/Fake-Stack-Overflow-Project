8b7f44890869b465024d7775b13991a11abd074f
Sahil <sahsahil1998@gmail.com>
2023-12-08 14:23:16 -0500

    working on use case 4 tests

--- server/init.js
+++ server/init.js
@@ -6,7 +6,6 @@ const Question = require('./models/questions');
 const Answer = require('./models/answers');
 const Comment = require('./models/comments');
 
-// Update the MongoDB URL if needed
 const mongoDB = 'mongodb://127.0.0.1:27017/fake_so';
 
 mongoose.connect(mongoDB, {
@@ -19,92 +18,146 @@ const saltRounds = 10;
 async function createInitialData() {
     try {
         // Hashing the passwords
-        const hashedPassword1 = await bcrypt.hash('password1', saltRounds);
-        const hashedPassword2 = await bcrypt.hash('password2', saltRounds);
+        const hashedPasswords = await Promise.all([
+            bcrypt.hash('password1', saltRounds),
+            bcrypt.hash('password2', saltRounds),
+            bcrypt.hash('password3', saltRounds),
+            bcrypt.hash('password4', saltRounds),
+            bcrypt.hash('password5', saltRounds)
+        ]);
 
         // Create initial users
-        const user1 = new User({
-            username: 'user1',
-            email: 'user1@example.com',
-            passwordHash: hashedPassword1,
-            reputationPoints: 10
-        });
-        const user2 = new User({
-            username: 'user2',
-            email: 'user2@example.com',
-            passwordHash: hashedPassword2,
-            reputationPoints: 20
-        });
-        await user1.save();
-        await user2.save();
+        const users = [
+            new User({ username: 'user1', email: 'user1@gmail.com', passwordHash: hashedPasswords[0], reputationPoints: 50 }),
+            new User({ username: 'user2', email: 'user2@gmail.com', passwordHash: hashedPasswords[1], reputationPoints: 20 }),
+            new User({ username: 'user3', email: 'user3@gmail.com', passwordHash: hashedPasswords[2], reputationPoints: 70 }),
+            new User({ username: 'user4', email: 'user4@gmail.com', passwordHash: hashedPasswords[3], reputationPoints: 30 }),
+            new User({ username: 'user5', email: 'user5@gmail.com', passwordHash: hashedPasswords[4], reputationPoints: 10 })
+        ];
+
+        await Promise.all(users.map(user => user.save()));
 
         // Create initial tags
-        const tag1 = new Tag({
-            name: 'JavaScript',
-            createdBy: user1._id
-        });
-        const tag2 = new Tag({
-            name: 'MongoDB',
-            createdBy: user2._id
-        });
-        await tag1.save();
-        await tag2.save();
+        const tags = [
+            new Tag({ name: 'JavaScript', createdBy: users[0]._id }),
+            new Tag({ name: 'MongoDB', createdBy: users[1]._id }),
+            new Tag({ name: 'React', createdBy: users[2]._id }),
+            new Tag({ name: 'Node.js', createdBy: users[3]._id }),
+            new Tag({ name: 'CSS', createdBy: users[4]._id })
+        ];
+
+        await Promise.all(tags.map(tag => tag.save()));
 
         // Create initial questions
-        const question1 = new Question({
-            title: 'How to use promises in JavaScript?',
-            text: 'I am having trouble understanding promises in JavaScript. Can someone help?',
-            tags: [tag1._id],
-            asked_by: user1._id,
-            summary: 'Understanding JavaScript promises',
-            answerCount: 1,
-            views: 5,
-            last_answered_time: new Date()
-        });
-        const question2 = new Question({
-            title: 'Best practices for MongoDB schema design?',
-            text: 'What are some best practices for designing MongoDB schemas?',
-            tags: [tag2._id],
-            asked_by: user2._id,
-            summary: 'MongoDB schema design best practices',
-            answerCount: 1,
-            views: 3,
-            last_answered_time: new Date()
-        });
-        await question1.save();
-        await question2.save();
+        const questions = [
+            new Question({
+                title: 'How to use promises in JavaScript?',
+                text: 'I am having trouble understanding promises in JavaScript. Can someone help?',
+                tags: [tags[0]._id],
+                asked_by: users[0]._id,
+                summary: 'Understanding JavaScript promises',
+                answerCount: 1,
+                views: 5,
+                ask_date_time: new Date('2023-01-01T08:00:00Z'),
+                last_answered_time: new Date('2023-01-02T08:00:00Z')
+            }),
+            new Question({
+                title: 'Best practices for MongoDB schema design?',
+                text: 'What are some best practices for designing MongoDB schemas?',
+                tags: [tags[1]._id],
+                asked_by: users[1]._id,
+                summary: 'MongoDB schema design best practices',
+                answerCount: 1,
+                views: 3,
+                ask_date_time: new Date('2023-01-03T08:00:00Z'),
+                last_answered_time: new Date('2023-01-04T08:00:00Z')
+            }),
+            new Question({
+                title: 'React State Management',
+                text: 'How do you effectively manage state in a large React application?',
+                tags: [tags[2]._id],
+                asked_by: users[2]._id,
+                summary: 'State management in React',
+                answerCount: 0, // No answers yet
+                views: 10,
+                ask_date_time: new Date('2023-01-05T08:00:00Z'),
+                last_answered_time: null // No answers yet
+            }),
+            new Question({
+                title: 'Node.js Best Practices',
+                text: 'What are some best practices for developing robust Node.js applications?',
+                tags: [tags[3]._id],
+                asked_by: users[3]._id,
+                summary: 'Best practices in Node.js',
+                answerCount: 0, // No answers yet
+                views: 7,
+                ask_date_time: new Date('2023-01-06T08:00:00Z'),
+                last_answered_time: null // No answers yet
+            }),
+            new Question({
+                title: 'CSS Grid vs Flexbox',
+                text: 'When should I use CSS Grid and when should I use Flexbox?',
+                tags: [tags[4]._id],
+                asked_by: users[4]._id,
+                summary: 'Understanding CSS layout tools',
+                answerCount: 2,
+                views: 15,
+                ask_date_time: new Date('2023-01-07T08:00:00Z'),
+                last_answered_time: new Date('2023-01-08T08:00:00Z')
+            }),
+            new Question({
+                title: 'Handling Async Operations in Redux',
+                text: 'What is the best way to handle asynchronous operations in Redux?',
+                tags: [tags[2]._id, tags[3]._id],
+                asked_by: users[2]._id,
+                summary: 'Async operations in Redux',
+                answerCount: 0, // No answers yet
+                views: 4,
+                ask_date_time: new Date('2023-01-09T08:00:00Z'),
+                last_answered_time: null // No answers yet
+            }),
+            // ... potentially more questions ...
+        ];
+
+        await Promise.all(questions.map(question => question.save()));
 
         // Create initial answers
-        const answer1 = new Answer({
-            question: question1._id,
-            text: 'Promises are used for asynchronous operations. Here is how you can use them...',
-            ans_by: user2._id,
-            ans_date_time: new Date()
-        });
-        const answer2 = new Answer({
-            question: question2._id,
-            text: 'In MongoDB, schema design depends on how you intend to query your data...',
-            ans_by: user1._id,
-            ans_date_time: new Date()
-        });
-        await answer1.save();
-        await answer2.save();
+        const answers = [
+            new Answer({
+                question: questions[0]._id,
+                text: 'Promises are used for asynchronous operations. Here is how you can use them.',
+                ans_by: users[1]._id,
+                ans_date_time: new Date()
+            }),
+            new Answer({
+                question: questions[1]._id,
+                text: 'In MongoDB, schema design depends on how you intend to query your data.',
+                ans_by: users[0]._id,
+                ans_date_time: new Date()
+            }),
+            // Add more answers here...
+        ];
+
+        await Promise.all(answers.map(answer => answer.save()));
 
         // Create initial comments
-        const comment1 = new Comment({
-            text: 'This is a great answer!',
-            commented_by: user1._id,
-            onQuestion: question1._id,
-            comment_date_time: new Date()
-        });
-        const comment2 = new Comment({
-            text: 'I have a follow-up question...',
-            commented_by: user2._id,
-            onAnswer: answer1._id,
-            comment_date_time: new Date()
-        });
-        await comment1.save();
-        await comment2.save();
+        const comments = [
+            new Comment({
+                text: 'This is a great answer!',
+                commented_by: users[0]._id,
+                onQuestion: questions[0]._id,
+                comment_date_time: new Date()
+            }),
+            new Comment({
+                text: 'I have a follow-up question.',
+                commented_by: users[1]._id,
+                onAnswer: answers[0]._id,
+                comment_date_time: new Date()
+            }),
+            // Add more comments here...
+        ];
+
+        await Promise.all(comments.map(comment => comment.save()));
 
         console.log('Initial data created successfully');
     } catch (error) {
--- testing/cypress/e2e/fakeso.cy.js
+++ testing/cypress/e2e/fakeso.cy.js
@@ -75,7 +75,7 @@ describe('Create Account Tests', () => {
             failOnStatusCode: false,
             body: {
                 username: 'uniqueUsername',
-                email: 'user2@example.com',
+                email: 'user2@gmail.com',
                 password: 'password123'
             }
         }).then((response) => {
@@ -86,7 +86,7 @@ describe('Create Account Tests', () => {
 
     it('validates email format', () => {
         cy.visit('http://localhost:3000/#/register');
-        cy.get('input[name="email"]').type('invalidEmail');
+        cy.get('input[name="email"]').type('invalidemail');
         cy.get('input[name="password"]').type('password123');
         cy.get('input[name="confirmPassword"]').type('password123');
 
@@ -131,10 +131,224 @@ describe('Create Account Tests', () => {
     
         cy.get('@windowAlert').should('have.been.calledWith', 'Passwords do not match');
     });
+
+    it('does not allow account creation with missing fields in API request', () => {
+        cy.request({
+            method: 'POST',
+            url: 'http://localhost:8000/api/users/register',
+            failOnStatusCode: false,
+            body: {
+                
+                password: 'password123'
+            }
+        }).then((response) => {
+            expect(response.status).to.eq(400);
+            expect(response.body).to.contains('All fields are required');
+        });
+    });
     
 
-    // Add more tests as needed
 });
+
+describe('Login Tests', () => {
+    beforeEach(() => {
+        
+        cy.exec('node ../server/init.js');
+    });
+
+    afterEach(() => {
+        
+        cy.exec('node ../server/destroy.js');
+    });
+
+    it('successfully navigates to the login page', () => {
+        cy.visit('http://localhost:3000');
+        cy.get('.welcome-button').contains('Login as Existing User').click();
+        cy.url().should('include', '/#/login');
+    });
+
+    it('allows a registered user (user1) to log in successfully', () => {
+        cy.visit('http://localhost:3000/#/login');
+        cy.get('input[name="username"]').type('user1'); 
+        cy.get('input[name="password"]').type('password1');
+        cy.get('form').submit();
+
+        // Handle alert for successful login
+        cy.on('window:alert', (text) => {
+            expect(text).to.contains('Login successful');
+        });
+
+        // Check URL to confirm redirection to home page
+        cy.url().should('include', '/#/home');
+    });
+    
+
+    it('gives feedback for an unregistered username', () => {
+        cy.visit('http://localhost:3000/#/login');
+        cy.get('input[name="username"]').type('unregisteredUser');
+        cy.get('input[name="password"]').type('pass');
+
+        // Prepare to handle the alert for login failure
+        cy.on('window:alert', (text) => {
+            expect(text).to.contains('Login failed');
+        });
+
+        cy.get('form').submit();
+        cy.url().should('include', '/#/login');
+    });
+
+    it('gives feedback for an incorrect password', () => {
+        cy.visit('http://localhost:3000/#/login');
+        cy.get('input[name="username"]').type('user1');
+        cy.get('input[name="password"]').type('password2');
+
+        // Prepare to handle the alert for login failure
+        cy.on('window:alert', (text) => {
+            expect(text).to.contains('Login failed');
+        });
+
+        cy.get('form').submit();
+        cy.url().should('include', '/#/login');
+    });
+
+    
+});
+
+describe('Logout Tests', () => {
+    beforeEach(() => {
+        // Initialize the database state before each test
+        cy.exec('node ../server/init.js');
+
+        // Login the user before each test
+        cy.visit('http://localhost:3000/#/login');
+        cy.get('input[name="username"]').type('user1');
+        cy.get('input[name="password"]').type('password1');
+        cy.get('form').submit();
+        cy.url().should('include', '/#/home');
+    });
+
+    afterEach(() => {
+    
+        cy.exec('node ../server/destroy.js');
+    });
+
+    it('allows a logged-in user to log out successfully', () => {describe('Logout Tests', () => {
+        beforeEach(() => {
+            cy.exec('node ../server/init.js');
+    
+            // Login the user before each test
+            cy.visit('http://localhost:3000/#/login');
+            cy.get('input[name="username"]').type('user1');
+            cy.get('input[name="password"]').type('password1');
+            cy.get('form').submit();
+            cy.url().should('include', '/#/home');
+        });
+    
+        afterEach(() => {
+            cy.exec('node ../server/destroy.js');
+        });
+    
+        it('allows a logged-in user to log out successfully', () => {
+            cy.get('button').contains('Logout').click();
+            cy.url().should('include', '/');
+        });
+    
+        it('displays logout button when user is logged in', () => {
+            cy.get('button').contains('Logout').should('be.visible');
+        });
+    
+        it('does not display logout button when user is not logged in', () => {
+            cy.get('button').contains('Logout').click(); // Logout first
+            // Wait for logout to complete
+            cy.url().should('include', '/');
+            // Revisit the page to ensure the state is reset
+            cy.visit('http://localhost:3000');
+            cy.get('button').contains('Logout').should('not.exist');
+            cy.get('button').contains('Register').should('be.visible');
+            cy.get('button').contains('Login').should('be.visible');
+        });
+    
+    });
+    
+        cy.get('button').contains('Logout').click();
+        cy.url().should('include', '/');
+    });
+
+    it('displays logout button when user is logged in', () => {
+        cy.get('button').contains('Logout').should('be.visible');
+    });
+
+    it('does not display logout button when user is not logged in', () => {
+        // Clear application state
+        cy.clearCookies();
+        cy.clearLocalStorage();
+    
+        // Visit the welcome page as a guest
+        cy.visit('http://localhost:3000');
+        cy.get('.welcome-button').contains('Continue as Guest').click();
+    
+        // Ensure the user is on the home page as a guest
+        cy.url().should('include', '/#/home');
+    
+        // Check that the Logout button does not exist
+        cy.get('button').contains('Logout').should('not.exist');
+    
+        // Check that the Register and Login buttons are visible
+        cy.get('button').contains('Register').should('be.visible');
+        cy.get('button').contains('Login').should('be.visible');
+    });
+    
+    
+});
+
+describe('Home Page Tests as Guest User', () => {
+    beforeEach(() => {
+        cy.exec('node ../server/init.js'); // Initialize the database
+        cy.visit('http://localhost:3000/#/home');
+    });
+
+    afterEach(() => {
+        cy.exec('node ../server/destroy.js'); // Clean up the database
+    });
+
+    it('loads the home page with default Newest view', () => {
+        cy.get('.main-top h1').should('contain', 'All Questions');
+        cy.get('.button-container .buttonDeco.active').should('contain', 'Newest');
+    });
+
+    it('displays the correct number of questions with all details', () => {
+        cy.get('.questionContainer .question-entry').should('have.length', 5);
+        cy.get('.questionContainer .question-entry').each(($el) => {
+            cy.wrap($el).find('.postTitle a').should('exist');
+            cy.wrap($el).find('.questionSummary').should('exist');
+            cy.wrap($el).find('.tags .tagButton').should('exist');
+            cy.wrap($el).find('.postStats p').should('exist');
+            cy.wrap($el).find('.lastActivity p').should('exist');
+        });
+    });
+
+    it('paginates to the next set of questions', () => {
+        cy.get('.pagination-controls button').contains('Next').click();
+        cy.get('.questionContainer .question-entry').should('have.length', 1);
+    });
+
+    it('sorts questions by Active view', () => {
+        cy.get('.button-container .buttonDeco').contains('Active').click();
+        cy.get('.button-container .buttonDeco.active').should('contain', 'Active');
+    });
+
+    it('sorts questions by Unanswered view', () => {
+        cy.get('.button-container .buttonDeco').contains('Unanswered').click();
+        cy.get('.button-container .buttonDeco.active').should('contain', 'Unanswered');
+    });
+
+    // Additional tests for error handling, navigation to question details, and other scenarios...
+});
+
+
+
+
+
 
 
 
